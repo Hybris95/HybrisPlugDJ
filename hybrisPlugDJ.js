ac725bf2092c744bbf1404832f052cd7b7dc2202
Hybris95 <hybris_95@hotmail.com>
2014-09-22 21:21:30 +0200

    Removed AntoAnswer/AutoRaffle since these are going to be fixed.

--- hybrisPlugDJ.js
+++ hybrisPlugDJ.js
@@ -45,71 +45,13 @@ function someoneLeft(user){
 /**
  * CHAT EVENT :
  * AutoNotice - http://pastebin.com/BVDgf6W0 -> http://pastebin.com/Hsi2YMDH
- * AutoAnswer (Just a Chill room)
- * AutoRaffle (Just a Chill room)
  */
 var autoNotice = false;
-var autoAnswer = false;
-var autoRaffle = false;
 
 var ownUserName = API.getUser().username;
 var lastTimeStamp = false;
 var loadedSound = new Audio(decodeURIComponent("https://gmflowplayer.googlecode.com/files/notify.ogg"));
 
-var AFK_MESSAGE_ONE = "you have been AFK for .+m, please respond within 2 minutes or you will be removed";
-var AFK_MESSAGE_TWO = ", you will be removed soon if you don't respond";
-var AFK_ANSWERS = ["@ChillBot Im not", "@ChillBot Im around", "Im not", "Im around"];
-
-var RAFFLE_MESSAGE = "Congratulations, @.* has won the raffle! Type !raffle to get boosted to spot";
-var RAFFLE_POSITIVE_ANSWERS = [":)", "I guess I won :)", "Thanks!", "Raffle!!", ":D", "@ChillBot I <3 you"];
-var RAFFLE_NEGATIVE_ANSWERS = ["@ChillBot I guess someone else can raffle on this ;)", "@ChillBot I pass my turn on this raffle", "@ChillBot Thanks but no thanks", "I guess someone else can raffle on this ;)", "I pass my turn on this raffle", "Thanks but no thanks"];
-
-function randNumber(min, max){
-    return Math.floor(Math.random() * (max - min) + min);
-}
-function iWonRaffle(){
-    var randAnswer = randNumber(0, RAFFLE_POSITIVE_ANSWERS.length - 1);
-    var answer = RAFFLE_POSITIVE_ANSWERS[randAnswer];
-    API.sendChat(answer);
-    if(debug)
-    {
-        console.log("Won Raffle victory message : " + answer);
-        console.log(randAnswer + "th answer");
-    }
-}
-function respondRaffle(){
-    if(API.getWaitListPosition() < 10)
-    {
-        var randAnswer = randNumber(0, RAFFLE_NEGATIVE_ANSWERS.length - 1);
-        var answer = RAFFLE_NEGATIVE_ANSWERS[randAnswer];
-        API.sendChat(answer);
-        if(debug)
-        {
-            console.log("Won raffle but passing the turn message : " + answer);
-            console.log(randAnswer + "th answer");
-        }
-    }
-    else
-    {
-        API.sendChat("!raffle");
-        var timeOut = randNumber(10000, 15000);
-        setTimeout(iWonRaffle, timeOut);
-        if(debug)
-        {
-            console.log("Autoraffle done, answer given in " + timeOut + "ms");
-        }
-    }
-}
-function imNotAfk(){
-    var randAnswer = randNumber(0, AFK_ANSWERS.length - 1);
-    var answer = AFK_ANSWERS[randAnswer];
-    API.sendChat(answer);
-    if(debug)
-    {
-        console.log("Answered as if not afk : " + answer);
-        console.log(randAnswer + "th answer");
-    }
-}
 function analyseChat(chat){
     var message = chat.message;
     var username = chat.un;
@@ -136,45 +78,6 @@ function analyseChat(chat){
         {
             loadedSound.play();
         }
-        if(username.match("ChillBot"))
-        {
-            // AutoAnswer
-            if(autoAnswer)
-            {
-                if(message.match(AFK_MESSAGE_ONE))
-                {
-                    var timeOut = randNumber(10000, 15000);
-                    setTimeout(imNotAfk, timeOut);
-                    if(debug)
-                    {
-                        console.log("Autoanswer asked in " + timeOut + "ms");
-                    }
-                }
-                else if(message.match(AFK_MESSAGE_TWO))
-                {
-                    var timeOut = randNumber(10000, 15000);
-                    setTimeout(imNotAfk, timeOut);
-                    if(debug)
-                    {
-                        console.log("Autoanswer asked in " + timeOut + "ms");
-                    }
-                }
-            }
-            
-            // AutoRaffle
-            if(autoRaffle)
-            {
-                if(message.match(RAFFLE_MESSAGE))
-                {
-                    var timeOut = randNumber(10000, 15000);
-                    setTimeout(respondRaffle, timeOut);
-                    if(debug)
-                    {
-                        console.log("Autoraffle asked in " + timeOut + "ms");
-                    }
-                }
-            }
-        }
     }
 }
 
@@ -194,7 +97,7 @@ function refreshAPIStatus()
     {
         API.on(API.ADVANCE, autowoot);
     }
-    if(autoAnswer || autoRaffle || autoNotice)
+    if(autoNotice)
     {
         API.on(API.CHAT, analyseChat);
     }
@@ -256,22 +159,6 @@ function switchAutoNotice(){
 }
 startAutoNotice();
 $("#chat-sound-button").bind('click', switchAutoNotice);
-function startAutoAnswer(){
-    autoAnswer = true;
-    refreshAPIStatus();
-}
-function stopAutoAnswer(){
-    autoAnswer = false;
-    refreshAPIStatus();
-}
-function startAutoRaffle(){
-    autoRaffle = true;
-    refreshAPIStatus();
-}
-function stopAutoRaffle(){
-    autoRaffle = false;
-    refreshAPIStatus();
-}
 function startAutoNoticeJoinersLeavers(){
     autoJoinNotice = true;
     autoLeaveNotice = true;
