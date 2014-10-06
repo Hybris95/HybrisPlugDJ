0da8cf0aa5dd52ee113a3de39141921ffa524e0e
Hybris95 <hybris_95@hotmail.com>
2014-10-06 11:57:39 +0200

    Passes to GPLv3 Licence

--- hybrisPlugDJ.js
+++ hybrisPlugDJ.js
@@ -1,6 +1,23 @@
-/**
-Copyright © Hybris95
-Contact : hybris_95@hotmail.com
+/** GNU GPLv3 Licensing :
+Christian BUISSON French Developper contact by electronic mail: hybris_95@hotmail.com
+Copyright © 2014 Christian BUISSON
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
+/** USAGE :
 Firefox:
 Add the following line as a bookmark :
 javascript:(function(){$.getScript('https://raw.githubusercontent.com/Hybris95/HybrisPlugDJ/master/hybrisPlugDJ.js');}());
